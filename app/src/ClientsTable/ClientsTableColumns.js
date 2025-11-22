b50204b92503916dc5aefa7a2c60f83d79962e92
zuko <zukonizim@gmail.com>
2025-11-22 20:00:41 +0300

    Add ClientsTableColumns for client data display

---
+++ app/src/ClientsTable/ClientsTableColumns.js
@@ -0,0 +1,37 @@
+ export const ClientsTableColumns = [
+  {
+    title: 'IP Address',
+    dataIndex: 'ip',
+    key: 'ip',
+  },
+  {
+    title: 'Tag',
+    dataIndex: 'tag',
+    key: 'tag',
+  },
+  {
+    title: 'User@PC',
+    dataIndex: 'username',
+    key: 'username',
+  },
+  {
+    title: 'RAM',
+    dataIndex: 'ram',
+    key: 'ram',
+  },
+  {
+    title: 'CPU',
+    dataIndex: 'cpu',
+    key: 'cpu',
+  },
+  {
+    title: 'Socket ID',
+    dataIndex: 'socket',
+    key: 'socket',
+  },
+  {
+    title: 'OS',
+    dataIndex: 'os',
+    key: 'os',
+  },
+];
